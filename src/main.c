ddbb8828f8c5bbad5fc3d9dc6acbfd377048cb7c
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2023-08-12 13:57:55 +0200

    add null terminator to the login func

--- src/main.c
+++ src/main.c
@@ -495,7 +495,7 @@ void login()
         sprintf(calcidStr + i * 2, "%02X", systemInfo->calcid[i]);
     }
     char login_msg[85];
-    snprintf(login_msg, sizeof(login_msg), "LOGIN:%s:%s:%s", calcidStr, username, authkey);
+    snprintf(login_msg, sizeof(login_msg), "LOGIN:%s:%s:%s\0", calcidStr, username, authkey);
     SendSerial(login_msg);
 }
 
